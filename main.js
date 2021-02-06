b1aff23ea7f7f06debb99d4b24f126332937e9c3
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-07 02:35:13 +0600

    added modal

--- main.js
+++ main.js
@@ -12,10 +12,8 @@ recipeCloseBtn.addEventListener('click', () => {
 
 // get meal list that matches with the ingredients
 function getMealList() {
-  let searchInputTxt = document.getElementById('search-input').value.trim();
-  fetch(
-    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`
-  )
+  let searchInput = document.getElementById('search-input').value;
+  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput}`)
     .then((response) => response.json())
     .then((data) => {
       let html = '';
@@ -44,17 +42,17 @@ function getMealList() {
 }
 
 // get recipe of the meal
-function getMealRecipe(e) {
-  e.preventDefault();
-  if (e.target.classList.contains('recipe-btn')) {
-    let mealItem = e.target.parentElement.parentElement;
-    fetch(
-      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`
-    )
-      .then((response) => response.json())
-      .then((data) => mealRecipeModal(data.meals));
-  }
-}
+// function getMealRecipe(e) {
+//   e.preventDefault();
+//   if (e.target.classList.contains('recipe-btn')) {
+//     let mealItem = e.target.parentElement.parentElement;
+//     fetch(
+//       `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`
+//     )
+//       .then((response) => response.json())
+//       .then((data) => mealRecipeModal(data.meals));
+//   }
+// }
 
 // create a modal
 function mealRecipeModal(meal) {
