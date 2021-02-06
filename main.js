7fb387bbb9acc238ad5e82925f114b0f053686bf
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-07 01:45:24 +0600

    added some line

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-const baseApi = 'https://www.themealdb.com/api/json/v1/1/search.php?f=';
-
-async function getCountyName(food) {
-  const responce = await fetch(`${baseApi}${food}`);
-  const data = await responce.json();
-  console.log(data);
-}
-
-// added button handler
-
-document.getElementById('search-button').addEventListener('click', function () {
-  const foodName = document.getElementById('input').value;
-  getCountyName(foodName);
-});
+fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+  .then((response) => response.json())
+  .then((data) => console.log(data.meals[0].strMeal));
