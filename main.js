bfedb6c2be0b7bfd71add3d08c3c5445faaaa8a1
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-07 02:54:28 +0600

    added some style

--- main.js
+++ main.js
@@ -42,17 +42,17 @@ function getMealList() {
 }
 
 // get recipe of the meal
-// function getMealRecipe(e) {
-//   e.preventDefault();
-//   if (e.target.classList.contains('recipe-btn')) {
-//     let mealItem = e.target.parentElement.parentElement;
-//     fetch(
-//       `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`
-//     )
-//       .then((response) => response.json())
-//       .then((data) => mealRecipeModal(data.meals));
-//   }
-// }
+function getMealRecipe(e) {
+  e.preventDefault();
+  if (e.target.classList.contains('recipe-btn')) {
+    let mealItem = e.target.parentElement.parentElement;
+    fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`
+    )
+      .then((response) => response.json())
+      .then((data) => mealRecipeModal(data.meals));
+  }
+}
 
 // create a modal
 function mealRecipeModal(meal) {
