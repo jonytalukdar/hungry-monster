1980678bd74b6f59e49a8a08d784383f8f7e38e3
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-07 19:53:08 +0600

    remove button

--- main.js
+++ main.js
@@ -27,7 +27,6 @@ function getMealList() {
                         </div>
                         <div class = "meal-name">
                             <h3>${meal.strMeal}</h3>
-                            <a href = "#" class = "recipe-btn">Get Recipe</a>
                         </div>
                     </div>
                 `;
