c0fd088b2a62edbb6bc7b432fb8c04975a03eee0
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-08 19:24:33 +0600

    remove some comments

--- test.js
+++ test.js
@@ -15,29 +15,7 @@ function getMealList() {
   let searchInput = document.getElementById('search-input').value[0];
   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${searchInput}`)
     .then((response) => response.json())
-    .then(
-      (data) => disPlay(data)
-      //   let html = '';
-      //   if (data.meals) {
-      //     data.meals.forEach((meal) => {
-      //       html += `
-      //                 <div class = "meal-item" data-id = "${meal.idMeal}">
-      //                     <div class = "meal-img">
-      //                         <img src = "${meal.strMealThumb}" alt = "food">
-      //                     </div>
-      //                     <div class = "meal-name">
-      //                         <h3>${meal.strMeal}</h3>
-      //                     </div>
-      //                 </div>
-      //             `;
-      //     });
-      //     mealList.classList.remove('notFound');
-      //   } else {
-      //     html = "Sorry, we didn't find any meal!";
-      //     mealList.classList.add('notFound');
-      //   }
-      //   mealList.innerHTML = html;
-    );
+    .then((data) => disPlay(data));
 }
 
 const disPlay = (foods) => {
